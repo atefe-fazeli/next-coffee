23d74c04912d409bed47d9174c512ce15b3f1e2d
atefe fazeli <atefefazeli1996@outlook.com>
2025-02-28 21:26:38 +0330

    slider completed

--- next-coffee-shop/pages/index.js
+++ next-coffee-shop/pages/index.js
@@ -1,8 +1,10 @@
+import Slider from "@/components/templates/Index/Slider";
 import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
     <>
+    <Slider/>
     </>
   );
 }
