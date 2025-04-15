7c5c9a0070adfe675cfb69ff43ee4b014b6a6e9c
atefe fazeli <atefefazeli1996@outlook.com>
2025-04-15 12:52:38 +0330

    about page completed

--- next-coffee-shop/pages/about/index.js
+++ next-coffee-shop/pages/about/index.js
@@ -1,6 +1,14 @@
 import PageHeader from "@/components/modules/PageHeader/PageHeader";
+import Story from "@/components/templates/About/Story";
+import React from "react";
 
-export default function About() {
-  return <>
-  <PageHeader title="About"/> </>;
+function About() {
+  return (
+    <>
+      <PageHeader title="About" />
+      <Story />
+    </>
+  );
 }
+
+export default About;
